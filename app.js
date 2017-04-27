5874f81644a945887544532a15e23adf6dd49bcc
Hendra Permana <hynra@yahoo.com>
2017-04-27 13:48:36 +0700

    notification listener

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var mongo = require('./database/mongo');
 var cluster = require('cluster');
+var amqp = require('amqplib/callback_api');
+var broker_uri = require('./setup').broker_setup.broker_uri;
 
-/*
 if (cluster.isMaster) {
     var cpuCount = require('os').cpus().length;
     for (var i = 0; i < cpuCount; i += 1) {
@@ -10,23 +11,46 @@ if (cluster.isMaster) {
     cluster.on('exit', function (worker) {
         console.log('Worker %d died :(', worker.id);
         cluster.fork();
-
     });
 
 }else {
-    mongo.connect().then(function (database) {
-        exports.database = database;
-        require('./tests/fcm_send_msg');
-    }).catch(function (err) {
-        console.log(err);
-    });
-
+    connectToBroker();
     console.log('Worker %d running!', cluster.worker.id);
-}*/
+}
 
+function connectToBroker() {
+    amqp.connect(broker_uri, function (err, conn) {
+        if (err) {
+            console.log("connect to broker err %s", err);
+            console.log("retry to connect in 5 secs ...");
+            setTimeout(function () {
+                connectToBroker();
+            }, 5000);
+        } else {
+            conn.createChannel(function (err, ch) {
+                if (err) {
+                    console.log("create channel err %s", err);
+                } else {
+                    console.log("success create channel ");
+                    exports.chnannel = ch;
+                    mongo.connect().then(function (database) {
+                        exports.database = database;
+
+                    }).catch(function (err) {
+                        console.log(err);
+                    });
+                }
+            });
+        }
+    });
+}
+
+
+/*
 mongo.connect().then(function (database) {
     exports.database = database;
     require('./tests/fcm_send_msg');
 }).catch(function (err) {
     console.log(err);
 });
+    */
---
+++ services/notification_listener.js
@@ -0,0 +1,38 @@
+var db = require('../app').database;
+var channel = require('../app').chnannel;
+var setup = require('../setup');
+var states = setup.broker_setup.states;
+
+const exchangeName = setup.broker_setup.exchange_name;
+const queueName = setup.broker_setup.queue_name;
+const queueTo = setup.broker_setup.consume_routing_key;
+
+function consume () {
+    channel.assertExchange(exchangeName, 'topic', {durable: false});
+    channel.prefetch(1);
+    channel.assertQueue(queueName, {exclusive: false}, function (err, q) {
+        if (err) {
+            console.log("error : %s",err);
+        } else {
+            channel.bindQueue(q.queue, exchangeName, queueTo);
+            channel.consume(q.queue, function (msg) {
+                console.log(msg.content.toString());
+                checkState(msg.fields.routingKey, msg);
+            }, {noAck: true});
+        }
+    });
+}
+
+function checkState(type, msg) {
+    switch (type){
+        case states.emergency_notification:
+
+            break;
+    }
+}
+
+
+module.exports = {
+    consume:consume
+};
+
